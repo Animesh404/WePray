e356d6105f8cf114ce27b398caa73455f4544b20
Grace Creatives <sumit.web6@gmail.com>
2025-01-28 17:17:27 +0530

    Subscribition Section update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -240,7 +240,7 @@ const Home = () => {
                 <svg className="w-5 h-5 mr-2 text-[#409F9C]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                 </svg>
-                Personal Prayer Journal
+                Optimized for mobile
               </li>
             </ul>
           </div>
