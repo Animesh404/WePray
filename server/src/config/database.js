26f01a221fa11ba4dea05acdfc6beb8ccea531e3
Animesh404 <akjha6680@gmail.com>
2025-02-01 10:38:18 +0530

    feat:stripe plan sync

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,18 +1,19 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-// const urlDB = process.env.MYSQL_URL
+// const MySQLStore = require('express-mysql-session')(session);
+const urlDB = process.env.MYSQL_URL
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'wepray',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME || 'wepray',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
 
-// const pool = mysql.createPool(urlDB);
+const pool = mysql.createPool(urlDB);
 
 
 module.exports = pool;
